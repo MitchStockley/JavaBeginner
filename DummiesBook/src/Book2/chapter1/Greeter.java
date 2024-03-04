bd37aabf3a6d4550e192260e461ee222875cdebc
Mitchell <mitchell.stockley@gmail.com>
2024-03-04 10:43:16 +0200

    dummies book started

---
+++ DummiesBook/src/Book2/chapter1/Greeter.java
@@ -0,0 +1,14 @@
+package Book2.chapter1;
+
+import javax.swing.*;
+
+public class Greeter {
+//    public void sayHello(){
+//        System.out.println("Hello world");
+//    }
+
+    public void sayHello(){
+        JOptionPane.showMessageDialog(null, "Hello world", "Greeter",
+                JOptionPane.INFORMATION_MESSAGE);
+    }
+}
